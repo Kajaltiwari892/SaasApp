abf2237fb51a47933066ac266ee2816baa84f3ea
Kajaltiwari892 <kanjal55567@gmail.com>
2025-10-01 18:48:18 +0530

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,51 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
+
+import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-      <Button>
-        Lets get started
-      </Button>
-    </div>
-      
-  )
-}
+    <main>
+      <h1 className="text-2xl underline">Popular Companion</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives $ Integrals"
+          subject="Maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocalbulary Bulder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionList 
+        title="Recently completed sessions"
+        companions={recentSessions}
+        classNames="w-2/3 max-lg:w-full"
+
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
 
 export default Page;
